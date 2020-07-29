6c4ad38ea540c5fc5027fc94d3cf66a24fe73712
mamingjie-China <mamingjie@baidu.com>
2020-07-29 15:22:24 +0800

    add split.py

---
+++ coco_split.py
@@ -0,0 +1,82 @@
+# copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os.path as osp
+import random
+import json
+import numpy as np
+
+
+class MyEncoder(json.JSONEncoder):
+    # 调整json文件存储形式
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+def split_coco_dataset(dataset_dir, val_percent, test_percent):
+    try:
+        from pycocotools.coco import COCO
+    except:
+        print(
+            "pycococotools is not installed, follow this doc install pycocotools: https://paddlex.readthedocs.io/zh_CN/develop/install.html#pycocotools"
+        )
+        return
+    annotation_file = osp.join(dataset_dir, "annotations.json")
+    coco = COCO(annotation_file)
+    img_ids = coco.getImgIds()
+    cat_ids = coco.getCatIds()
+    anno_ids = coco.getAnnIds()
+
+    val_num = int(len(img_ids) * val_percent)
+    test_num = int(len(img_ids) * test_percent)
+    train_num = len(img_ids) - val_num - test_num
+
+    random.shuffle(img_ids)
+    train_files_ids = img_ids[:train_num]
+    val_files_ids = img_ids[train_num:train_num + val_num]
+    test_files_ids = img_ids[train_num + val_num:]
+
+    for img_id_list in [train_files_ids, val_files_ids, test_files_ids]:
+        img_anno_ids = coco.getAnnIds(imgIds=img_id_list, iscrowd=0)
+        imgs = coco.loadImgs(img_id_list)
+        instances = coco.loadAnns(img_anno_ids)
+        categories = coco.loadCats(cat_ids)
+        img_dict = {
+            "annotations": instances,
+            "images": imgs,
+            "categories": categories
+        }
+
+        if img_id_list == train_files_ids:
+            json_file = open(osp.join(dataset_dir, 'train.json'), 'w+')
+            json.dump(img_dict, json_file, cls=MyEncoder)
+        elif img_id_list == val_files_ids:
+            json_file = open(osp.join(dataset_dir, 'val.json'), 'w+')
+            json.dump(img_dict, json_file, cls=MyEncoder)
+        elif img_id_list == test_files_ids:
+            json_file = open(osp.join(dataset_dir, 'test.json'), 'w+')
+            json.dump(img_dict, json_file, cls=MyEncoder)
+    print("COCO Split Done")
+    print("Train samples: {}".format(train_num))
+    print("Eval samples: {}".format(val_num))
+    print("Test samples: {}".format(test_num))
+    print("Split json(train.json, val.json, test.json) saved in {}".format(
+        dataset_dir))
---
+++ split.py
@@ -0,0 +1,95 @@
+# copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from six import text_type as _text_type
+import argparse
+import sys
+import os.path as osp
+from coco_split import split_coco_dataset
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--type",
+        "-t",
+        type=_text_type,
+        default=None,
+        help="the type of dataset(COCO, ImageNet)")
+    parser.add_argument(
+        "--dir",
+        "-d",
+        type=_text_type,
+        default=None,
+        help="the path of dataset to be splited")
+    parser.add_argument(
+        "--val_percent",
+        "-vp",
+        type=_text_type,
+        default=None,
+        help="validation set percent number(for example 0.2)")
+    parser.add_argument(
+        "--test_percent",
+        "-tp",
+        type=_text_type,
+        default=None,
+        help="test set percent number(for example 0.1)")
+
+    return parser
+
+
+def check_input(dataset_type, dataset_dir, val_percent, test_percent):
+    # 输入校验
+    if not dataset_type in ["coco", "imagenet"]:
+        raise ValueError(
+            "--type is not correct defined(support COCO/ImageNet)")
+    if not osp.exists(dataset_dir):
+        raise ValueError("File {} is not exist!".format(dataset_dir))
+    if val_percent <= 0 or val_percent >= 1 or test_percent < 0 or test_percent >= 1 or val_percent + test_percent >= 1 - 0.00001:
+        raise ValueError("Please input correct split percent")
+
+    if dataset_type == "coco":
+        if not osp.exists(osp.join(dataset_dir, "annotations.json")):
+            raise ValueError("\'annotations.json\' is not found in {}!".format(
+                dataset_dir))
+    elif dataset_type == "imagenet":
+        if not osp.exists(osp.join(dataset_dir, "JPEGImages")):
+            raise ValueError("\'JPEGImages\' is not found in {}!".format(
+                dataset_dir))
+
+    return True
+
+
+def main():
+    if len(sys.argv) < 4:
+        print(
+            "Usage: python split.py --type COCO --dir ann.json --val_percent 0.2 --test_percent 0.1"
+        )
+        return
+
+    parser = arg_parser()
+    args = parser.parse_args()
+
+    dataset_type = args.type.lower()
+    dataset_dir = args.dir
+    val_percent = float(args.val_percent)
+    test_percent = float(args.test_percent)
+
+    if check_input(dataset_type, dataset_dir, val_percent, test_percent):
+        if dataset_type == "coco":
+            split_coco_dataset(dataset_dir, val_percent, test_percent)
+
+
+if __name__ == "__main__":
+    main()
